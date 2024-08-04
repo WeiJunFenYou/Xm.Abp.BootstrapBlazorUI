8a56a269e4f44d2c1e7e87b39bbc172464410edd
WeiJunFenYou <tdxh1357@163.com>
2024-08-04 22:47:54 +0800

    [ADD] 添加账户模块

---
+++ modules/Account/Xm.Abp.Account.Web/Areas/Account/Controllers/Models/AbpLoginResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Xm.Abp.Account.Web.Areas.Account.Controllers.Models;
+
+public class AbpLoginResult
+{
+    public AbpLoginResult(LoginResultType result)
+    {
+        Result = result;
+    }
+
+    public LoginResultType Result { get; }
+
+    public string Description => Result.ToString();
+}
